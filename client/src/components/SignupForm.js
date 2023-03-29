01a5eda86a07d83919b1309afe1d93d40d59f82f
Sarah Jacobs <sgjacobs14@gmail.com>
2023-03-29 16:43:22 -0400

    still working on bug

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 // import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 
 const SignupForm = () => {
   // set initial form state
